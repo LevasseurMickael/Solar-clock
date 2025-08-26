3119d6bdd49965787fb423635d8ee7dc2e6ba311
Levasseur Mickael <Stwompy@gmail.com>
2025-08-26 11:00:06 +0200

    clock working

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,53 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+
+
+
+
+
+let clockNumbersShown  //clock seen on screen
+
+
+//Clock overall time function
+function clockNumbers () {
+  const allTime = new Date();
+  let hourTime;
+  let minuteTime;
+  let secondTime;
+  if (allTime.getHours() < 10) {
+    hourTime = `0${allTime.getHours()}`;
+  } else {
+    hourTime = allTime.getHours();
+  };
+  if (allTime.getMinutes() < 10) {
+    minuteTime = `0${allTime.getMinutes()}`;
+  } else {
+    minuteTime = allTime.getMinutes();
+  };
+  if (allTime.getSeconds() < 10) {
+    secondTime = `0${allTime.getSeconds()}`;
+  } else {
+    secondTime = allTime.getSeconds();
+  };
+  clockNumbersShown = `${hourTime} : ${minuteTime} : ${secondTime}`;
+  return clockNumbersShown;
+}
+
+clockNumbers();
+setInterval(clockNumbers, 1000);
+
+
 </script>
 
 <main>
-
+  {clockNumbersShown}
 </main>
 
 <style>
-
+main {
+  background-color: beige;
+  color: black;
+  font-size: 2rem;
+}
 </style>
