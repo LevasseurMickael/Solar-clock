eb13fa34fab1dceb234cdd38fff109e99e1d7458
Levasseur Mickael <Stwompy@gmail.com>
2025-08-26 11:38:34 +0200

    clock changed to manually made clock

--- src/App.svelte
+++ src/App.svelte
@@ -10,31 +10,26 @@
 let clockNumbersShown  //clock seen on screen
 
 
-//Clock overall time function
+//Clock Earth time
 function clockNumbers () {
-  const allTime = new Date();
-  let hourTime;
-  let minuteTime;
-  let secondTime;
-  if (allTime.getHours() < 10) {
-    hourTime = `0${allTime.getHours()}`;
-  } else {
-    hourTime = allTime.getHours();
-  };
-  if (allTime.getMinutes() < 10) {
-    minuteTime = `0${allTime.getMinutes()}`;
-  } else {
-    minuteTime = allTime.getMinutes();
-  };
-  if (allTime.getSeconds() < 10) {
-    secondTime = `0${allTime.getSeconds()}`;
-  } else {
-    secondTime = allTime.getSeconds();
-  };
-  clockNumbersShown = `${hourTime} : ${minuteTime} : ${secondTime}`;
+  const currentTimeEartUtc = new Date();
+  const earthTimeUtcsecond = (currentTimeEartUtc.getTime()) / 1000;
+  const planetTime = earthTimeUtcsecond * 1;  //TODO change *1 by planets ratio
+  const currentPlanetTime = planetTime  //TODO change 1 by number of second per planet day
+  const hoursOnPlanet = Math.floor((currentPlanetTime % 86400) / 3600);
+  const minutesOnPlanet = Math.floor((currentPlanetTime %3600) / 60);
+  const secondOnPlanet = Math.floor(currentPlanetTime % 60);
+  const padWith0 = n => n.toString().padStart(2, "0");
+  clockNumbersShown = `${padWith0(hoursOnPlanet)} : ${padWith0(minutesOnPlanet)} : ${padWith0(secondOnPlanet)}`;
   return clockNumbersShown;
 }
 
+
+
+
+
+
+// Time refresh set to 1s
 clockNumbers();
 setInterval(clockNumbers, 1000);
 
